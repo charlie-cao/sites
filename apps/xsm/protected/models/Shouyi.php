e33e74a19f4b65900a642f1b0d7a351a80187fbf
Administrator <Administrator@PC201305281121>
2014-01-20 17:40:58 +0800

    增加用户关联

--- apps/xsm/protected/models/Shouyi.php
+++ apps/xsm/protected/models/Shouyi.php
@@ -44,6 +44,7 @@ class Shouyi extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+                    'user' => array(self::BELONGS_TO, 'User', 'uid'),
 		);
 	}
 
